06d3e2d2dfc936bf03381f17871f7691ab0ef3de
AsadNaqvi360 <asad.naqvi@ontariotechu.net>
2024-11-23 23:57:04 -0500

    working on a home page

--- GymApp/routes/workouts.js
+++ GymApp/routes/workouts.js
@@ -31,4 +31,42 @@ router.post('/', async (req, res) => {
     }
 });
 
+// GET: Fetch a workout by ID for editing
+router.get('/edit/:id', async (req, res) => {
+    try {
+        const workout = await Workout.findById(req.params.id);
+        res.render('editWorkout', { workout });
+    } catch (err) {
+        res.status(500).send('Server Error');
+    }
+});
+
+// POST: Update a workout
+router.post('/edit/:id', async (req, res) => {
+    const { name, type, sets, reps, restTime } = req.body;
+
+    try {
+        await Workout.findByIdAndUpdate(req.params.id, {
+            name,
+            type,
+            sets,
+            reps,
+            restTime,
+        });
+        res.redirect('/workouts');
+    } catch (err) {
+        res.status(500).send('Server Error');
+    }
+});
+
+// POST: Delete a workout
+router.post('/delete/:id', async (req, res) => {
+    try {
+        await Workout.findByIdAndDelete(req.params.id);
+        res.redirect('/workouts');
+    } catch (err) {
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
